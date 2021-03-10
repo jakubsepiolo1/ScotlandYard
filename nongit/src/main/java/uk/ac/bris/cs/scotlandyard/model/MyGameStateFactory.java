5b035991a746318585774e4400759cf01965d894
Jakub <cx20690@bristol.ac.uk>
2021-03-10 11:06:52 +0000

    stuff

---
+++ nongit/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -0,0 +1,85 @@
+package uk.ac.bris.cs.scotlandyard.model;
+
+import com.google.common.collect.ImmutableList;
+
+import javax.annotation.Nonnull;
+
+import com.google.common.collect.ImmutableSet;
+import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.Optional;
+
+/**
+ * cw-model
+ * Stage 1: Complete this class
+ */
+public final class MyGameStateFactory implements Factory<GameState> {
+
+	@Nonnull @Override public GameState build(
+			GameSetup setup,
+			Player mrX,
+			ImmutableList<Player> detectives) {
+			return new MyGameState(setup, ImmutableSet.of(MrX.MRX), ImmutableList.of(), mrX, detectives);
+			}
+		private final class MyGameState implements GameState {
+			private GameSetup setup;
+			private ImmutableSet<Piece> remaining;
+			private ImmutableList<LogEntry> log;
+			private Player mrX;
+			private List<Player> detectives;
+			private ImmutableList<Player> everyone;
+			private ImmutableSet<Move> moves;
+			private ImmutableSet<Piece> winner;
+			private MyGameState( final GameSetup setup,
+								 final ImmutableSet<Piece> remaining,
+								 final ImmutableList<LogEntry> log,
+								 final Player mrX,
+								 final List<Player> detectives){
+			this.setup = setup;
+			this.remaining = remaining;
+			this.log = log;
+			this.mrX = mrX;
+			this.detectives = detectives;
+
+			}
+
+
+			@Override public GameSetup getSetup() {
+				return null;
+			}
+			@Override  public ImmutableSet<Piece> getPlayers() {
+				return null;
+			}
+			@Nonnull
+			@Override
+			public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+				return Optional.empty();
+			}
+			@Nonnull
+			@Override
+			public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+				return Optional.empty();
+			}
+			@Nonnull
+			@Override
+			public ImmutableList<LogEntry> getMrXTravelLog() {
+				return null;
+			}
+			@Nonnull
+			@Override
+			public ImmutableSet<Piece> getWinner() {
+				return null;
+			}
+			@Nonnull
+			@Override
+			public ImmutableSet<Move> getAvailableMoves() {
+				return null;
+			}
+			@Override public GameState advance(Move move) {
+				return null;
+			}
+
+	}
+
+}
