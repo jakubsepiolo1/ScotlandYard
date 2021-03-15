a74ce3ed54006bde80c4497c2e2e1b8c8dc23cdc
Jakub <cx20690@bristol.ac.uk>
2021-03-15 14:44:00 +0000

    merge attempt

--- nongit/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ nongit/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -14,13 +14,13 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
  * Stage 1: Complete this class
  */
 public final class MyGameStateFactory implements Factory<GameState> {
-	testing tesing
 
 	@Nonnull @Override public GameState build(
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
 			return new MyGameState(setup, ImmutableSet.of(MrX.MRX), ImmutableList.of(), mrX, detectives);
+
 			}
 		private final class MyGameState implements GameState {
 			private GameSetup setup;
@@ -31,57 +31,110 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			private ImmutableList<Player> everyone;
 			private ImmutableSet<Move> moves;
 			private ImmutableSet<Piece> winner;
-			private MyGameState( final GameSetup setup,
-								 final ImmutableSet<Piece> remaining,
-								 final ImmutableList<LogEntry> log,
-								 final Player mrX,
-								 final List<Player> detectives){
-				if (mrX == null){throw new NullPointerException();}
+			private ImmutableSet<SingleMove> makeSingleMoves(
+					GameSetup setup,
+					List<Player> detectives,
+					Player player,
+					int source){
+				final var singleMoves = new ArrayList<SingleMove>();
+				for(int destination : setup.graph.adjacentNodes(source)) {
+					for(Player detective: detectives){
+						if(detective.location() == destination){
+							// need to fix this
+							break;
+						}
 
-			this.setup = setup;
-			this.remaining = remaining;
-			this.log = log;
-			this.mrX = mrX;
-			this.detectives = detectives;
+
+					}
+
+					//
+					for(Transport t : setup.graph.edgeValueOrDefault(source,destination,ImmutableSet.of())) {
+						if(player.has(t.requiredTicket())){
+							SingleMove newMove = new SingleMove(player.piece(), source, t.requiredTicket(), destination);
+
+							singleMoves.add(newMove);
+						}
+
+					}
+					// TODO consider the rules of secret moves here
+					//  add moves to the destination via a secret ticket if there are any left with the player
+				}
+				return ImmutableSet.copyOf(singleMoves);
+			}
+
+
+			private MyGameState(final GameSetup setup,
+								final ImmutableSet<Piece> remaining,
+								final ImmutableList<LogEntry> log,
+								final Player mrX,
+								final List<Player> detectives) {
+				if (mrX == null) {
+					throw new NullPointerException();
+				}
+				this.setup = setup;
+				this.remaining = remaining;
+				this.log = log;
+				this.mrX = mrX;
+				this.detectives = detectives;
 
 			}
 
 
-			@Override public GameSetup getSetup() {
+			@Override
+			public GameSetup getSetup() {
+				return setup;
+			}
+
+			@Override
+			public ImmutableSet<Piece> getPlayers() {
 				return null;
 			}
-			@Override  public ImmutableSet<Piece> getPlayers() {
-				return null;
-			}
+
 			@Nonnull
 			@Override
 			public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+				if(detective.isDetective()){
+					for(Player detective1: detectives){
+						if (detective1.piece() == detective){return Optional.of(detective1.location());}
+						}
+
+
+
+
+				}
+
 				return Optional.empty();
 			}
+
 			@Nonnull
 			@Override
 			public Optional<TicketBoard> getPlayerTickets(Piece piece) {
 				return Optional.empty();
 			}
+
 			@Nonnull
 			@Override
 			public ImmutableList<LogEntry> getMrXTravelLog() {
-				return null;
+				return log;
 			}
+
 			@Nonnull
 			@Override
 			public ImmutableSet<Piece> getWinner() {
 				return null;
 			}
+
 			@Nonnull
 			@Override
 			public ImmutableSet<Move> getAvailableMoves() {
 				return null;
 			}
-			@Override public GameState advance(Move move) {
+
+			@Override
+			public GameState advance(Move move) {
 				return null;
 			}
 
-	}
+		}
 
 }
