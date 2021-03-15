d0f1783c25427a628a58ad4a264cedb0a4f82ff3
Ben <benbrowne7@icloud.com>
2021-03-15 14:54:39 +0000

    70%FirstTestSet

--- nongit/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ nongit/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -2,19 +2,27 @@ package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.graph.ImmutableValueGraph;
 import java.util.*;
 import javax.annotation.Nonnull;
+
+import com.google.common.graph.MutableValueGraph;
+import com.google.common.graph.ValueGraphBuilder;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.Move.*;
 import uk.ac.bris.cs.scotlandyard.model.Piece.*;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
+
+import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.DETECTIVE_LOCATIONS;
+import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.STANDARD24ROUNDS;
+
 
 /**
  * cw-model
  * Stage 1: Complete this class
  */
 public final class MyGameStateFactory implements Factory<GameState> {
-	testing tesing
+
 
 	@Nonnull @Override public GameState build(
 			GameSetup setup,
@@ -36,7 +44,65 @@ public final class MyGameStateFactory implements Factory<GameState> {
 								 final ImmutableList<LogEntry> log,
 								 final Player mrX,
 								 final List<Player> detectives){
-				if (mrX == null){throw new NullPointerException();}
+
+
+
+				if (detectives == null) {throw new NullPointerException();}
+				if (mrX == null) {throw new NullPointerException();}
+				if (!(mrX.isMrX())) {throw new IllegalArgumentException();}
+
+				for (int i=0; i < detectives.size(); i++) {
+					if ((detectives.get(i)).isMrX()) {throw new IllegalArgumentException();}
+				}
+				String xcolour = (mrX.piece().webColour());
+				if (!(xcolour.equals("#000"))) {throw new IllegalArgumentException();}
+
+				for (int i=0; i < detectives.size()-1; i++) {
+					for (int j=1; j < detectives.size(); j++) {
+						if ((detectives.get(i)).equals(detectives.get(j))) {throw new IllegalArgumentException();}
+						else if ((detectives.get(i).location()) == (detectives.get(j).location())) {throw new IllegalArgumentException();}
+					}
+				}
+
+
+				for (int i=0; i < detectives.size(); i++) {
+					if ((detectives.get(i)).has(Ticket.SECRET)) {throw new IllegalArgumentException();}
+					else if ((detectives.get(i)).has(Ticket.DOUBLE)) {throw new IllegalArgumentException();}
+				}
+
+
+				if (((setup.rounds).isEmpty())) {throw new IllegalArgumentException();}
+				if (setup.graph.nodes().isEmpty()) {throw new IllegalArgumentException();}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 			this.setup = setup;
 			this.remaining = remaining;
@@ -44,18 +110,25 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			this.mrX = mrX;
 			this.detectives = detectives;
 
+
+
 			}
 
 
 			@Override public GameSetup getSetup() {
-				return null;
+				return setup;
 			}
 			@Override  public ImmutableSet<Piece> getPlayers() {
-				return null;
+				return remaining;
 			}
 			@Nonnull
 			@Override
-			public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			public Optional<Integer> getDetectiveLocation(Detective detective) {
+				for (int i=0; i < detectives.size(); i++) {
+					if (((detectives.get(i)).piece()).equals(detective)) {
+						return Optional.of((detectives.get(i)).location());
+					}
+				}
 				return Optional.empty();
 			}
 			@Nonnull
@@ -66,7 +139,7 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			@Nonnull
 			@Override
 			public ImmutableList<LogEntry> getMrXTravelLog() {
-				return null;
+				return log;
 			}
 			@Nonnull
 			@Override
@@ -83,5 +156,6 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			}
 
 	}
+
 
 }
